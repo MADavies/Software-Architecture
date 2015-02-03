0b500a13cbe646ca785414ab3737c7d7bf6a2bd8
Marc Davies <marc_davies@hotmail.com>
2015-02-03 14:39:43 +0000

    Created MVC Web Application
    Added MVC Web application and Unit Tests. Additionally web application
    utilises individual user accounts authentication to allow for user
    accounts.

---
+++ ICA1/ICA1WebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ICA1WebApplication.MvcApplication" Language="C#" %>
---
+++ ICA1/ICA1WebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ICA1WebApplication.Startup))]
+namespace ICA1WebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
