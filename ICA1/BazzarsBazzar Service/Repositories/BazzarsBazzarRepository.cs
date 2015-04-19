a1dc99ee66bd5c325ad44eab53838efe24046b52
Marc Davies <marc_davies@hotmail.com>
2015-04-19 19:41:22 +0100

    BazzasBazzarWCF Service Added
    Added the WCF Service Repo, not sure if this is done right if someone
    could take a look at this that would be great..

--- ICA1/BazzarsBazzar Service/Repositories/BazzarsBazzarRepository.cs
+++ ICA1/BazzarsBazzar Service/Repositories/BazzarsBazzarRepository.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using DavisonModel;
+using BazzarsBazzar_Service.BazzasBazzarWCFService;
 
 namespace BazzarsBazzar_Service.Repositories
 {
+    /**
+     * Handles data brought back from BazzasBazzar WCF Service
+     */ 
     public class BazzarsBazzarRepository : IBazzarsBazzarRepository, IDisposable
     {
-        public string WebApiUrl = "http://bazzasbazaar.cloudapp.net/Store.svc";
 
-        public IEnumerable<Base> GetAllProducts()
+        public IEnumerable<DavisonModel.Product> GetAllProducts()
         {
-            throw new NotImplementedException();
+            StoreClient BazzarsClient = new StoreClient();
+            BazzasBazzarWCFService.Product BazzasProducts = BazzarsClient.GetProductById(0);
+            BazzarsClient.Close();
+            return (IEnumerable<DavisonModel.Product>) BazzasProducts;
+            
         }
 
         public Base GetProduct(int id)
