6ac375e92c923f6059583b1fbf7ab152758fdb5e
DanielleHeywood <l1024006@live.tees.ac.uk>
2015-04-24 16:13:19 +0100

    Working version

---
+++ ICA1/DavisonService/Respositories/BazzasBazzarRepository.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DavisonService.BazzasBazzar;
+using DavisonService.ViewModels;
+
+namespace DavisonService.Respositories
+{
+    public class BazzasBazzarRepository
+    {
+        private StoreClient client = new StoreClient();
+
+        public IEnumerable<ProdVM> GetAllFiltered(int? id, string catName)
+        {
+            return client.GetFilteredProducts(id, catName, null, null)
+                .Select(p => new ProdVM
+                {
+                    Id = p.Id,
+                    EAN = p.Ean,
+                    CategoryId = p.CategoryId,
+                    Name = p.Name,
+                    Description = p.Description,
+                    Price = p.PriceForOne
+                }).AsEnumerable();
+        }
+
+        public IEnumerable<ProdVM> GetAllProducts()
+        {
+            return client.GetFilteredProducts(null, null, null, null)
+                .Select(p => new ProdVM
+                {
+                    Id = p.Id,
+                    EAN = p.Ean,
+                    CategoryId = p.CategoryId,
+                    Name = p.Name,
+                    Description = p.Description,
+                    Price = p.PriceForOne
+                }).AsEnumerable();
+        }
+
+        public Product GetProductDetails(int id)
+        {
+            return client.GetProductById(id);
+        }
+
+        public IEnumerable<CatVM> GetAllCat()
+        {
+            return client.GetAllCategories()
+                .Select(c => new CatVM
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                }).AsEnumerable();
+        }
+
+        public Category GetCatDetails(int id)
+        {
+            return client.GetCategoryById(id);
+        }
+    }
+}
---
+++ ICA1/DavisonService/Respositories/UnderCuttersRepository.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Diagnostics;
+using Davison.Model;
+
+namespace DavisonService.Respositories
+{
+    public class UnderCuttersRepository : IRepository
+    {
+        private HttpClient client;
+
+        public UnderCuttersRepository()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new System.Uri("http://under-cutters.azurewebsites.net/");
+            client.DefaultRequestHeaders.Accept.ParseAdd("application/json");
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            HttpResponseMessage response = client.GetAsync("api/product").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return response.Content.ReadAsAsync<IEnumerable<Product>>().Result;
+            }
+            else
+            {
+                Debug.WriteLine("Index received a bad response from the web service.");
+                return null;
+            }
+        }
+
+        public IEnumerable<Product> GetAllFiltered(TypeEnum? type, int? id)
+        {
+            HttpResponseMessage response = client.GetAsync("api/product/" + type + "/" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return response.Content.ReadAsAsync<IEnumerable<Product>>().Result;
+            }
+            else
+            {
+                Debug.WriteLine("Index received a bad response from the web service.");
+                return null;
+            }
+        }
+
+        public Product GetProductDetails(int? id)
+        {
+            HttpResponseMessage response = client.GetAsync("api/product/" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return response.Content.ReadAsAsync<Product>().Result;
+            }
+            else
+            {
+                Debug.WriteLine("Index received a bad response from the web service.");
+                return null;
+            }
+        }
+
+        public IEnumerable<Category> GetAllCat()
+        {
+            HttpResponseMessage response = client.GetAsync("api/category").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return response.Content.ReadAsAsync<IEnumerable<Category>>().Result;
+            }
+            else
+            {
+                Debug.WriteLine("Index received a bad response from the web service.");
+                return null;
+            }
+        }
+
+        public Category GetCatDetails(int? id)
+        {
+            HttpResponseMessage response = client.GetAsync("api/category/" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return response.Content.ReadAsAsync<Category>().Result;
+            }
+            else
+            {
+                Debug.WriteLine("Index received a bad response from the web service.");
+                return null;
+            }
+        }
+
+        public IEnumerable<Brand> GetAllBrand()
+        {
+            HttpResponseMessage response = client.GetAsync("api/brand").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return response.Content.ReadAsAsync<IEnumerable<Brand>>().Result;
+            }
+            else
+            {
+                Debug.WriteLine("Index received a bad response from the web service.");
+                return null;
+            }
+        }
+
+        public Brand GetBrandDetails(int? id)
+        {
+            HttpResponseMessage response = client.GetAsync("api/brand/" + id).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return response.Content.ReadAsAsync<Brand>().Result;
+            }
+            else
+            {
+                Debug.WriteLine("Index received a bad response from the web service.");
+                return null;
+            }
+        }
+
+    }
+}
