52ec32ee336c1d9509505a769053806c850966b1
Marc Davies <marc_davies@hotmail.com>
2015-04-30 21:25:31 +0100

    Added Restocker
    Added the restocker and its controllers but its not tied to anything at
    this time.

---
+++ ICA1/DavisonRestocker/Controllers/DeliveryRestockerController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Davison;
+using System.Threading.Tasks;
+
+namespace DavisonRestocker.Controllers
+{
+    public class DeliveryRestockerController : ApiController
+    {
+        private readonly Davison.Stocker.Stocker restocker;
+
+        //Constructor
+        public DeliveryRestockerController()
+        {
+            restocker = new Davison.Stocker.Stocker();
+        }
+
+        // GET api/DeliveryRestocker
+        public IEnumerable<Davison.Stocker.Delivery> GetDelivery(string id)
+        {
+            return restocker.QueryDeliveriesByProduct(id);
+        }
+
+        //POST: api/DeliveryRestocker
+        public async Task<bool> confirm([FromBody] Davison.Stocker.Delivery delivery) 
+        {
+            restocker.ConfirmDeliveryForProduct(delivery.Id);
+            return true;
+        }
+
+        // GET: api/DeliveryRestocker
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/DeliveryRestocker/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/DeliveryRestocker
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/DeliveryRestocker/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/DeliveryRestocker/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ICA1/DavisonRestocker/Controllers/RestockerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Davison;
+
+
+
+namespace DavisonRestocker.Controllers
+{
+    public class RestockerController : ApiController
+    {
+        private readonly Davison.Stocker.Stocker restocker;
+
+        //Constructor
+        public RestockerController()
+        {
+            restocker = new Davison.Stocker.Stocker();
+
+        }
+
+        // GET api/Restocker/{id}
+        public Davison.Stocker.Product Get(string id)
+        {
+            return restocker.GetProduct(id);
+        }
+
+    }
+}
