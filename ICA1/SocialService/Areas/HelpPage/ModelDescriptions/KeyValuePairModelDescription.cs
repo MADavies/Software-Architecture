7296a37255cdd4796dbe40c2795a1cad0aed8f4b
jenkirtley <l1024329@live.tees.ac.uk>
2015-04-30 11:14:41 +0100

    SocialService

---
+++ ICA1/SocialService/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace SocialService.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
